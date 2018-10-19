3735608bc36beeea99d17d4ebd861ce228680542
Barry Thach <bthach90@gmail.com>
2018-10-19 11:46:13 -0700

    Refactoring Routes

---
+++ YelpCamp/v7/routes/comments.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var router = express.Router();
+var Hike = require("../models/hike");
+var Comment = require("../models/comment");
+
+router.get("/new", isLoggedIn, function(req, res) {
+    Hike.findById(req.params.id, function(err, hike) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", { hike: hike });
+        }
+    })
+})
+
+router.post("/comments", isLoggedIn, function(req, res) {
+    Hike.findById(req.params.id, function(err, hike) {
+        if (err) {
+            console.log(err);
+            res.redirect("/hikes");
+        } else {
+            Comment.create(req.body.comment, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    hike.comments.push(comment);
+                    hike.save();
+                    res.redirect("/hikes/" + hike._id);
+                }
+            });
+
+        }
+    });
+})
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports =router;
---
+++ YelpCamp/v7/routes/hikes.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var router = express.Router();
+var Hike = require("../models/hike");
+
+router.get("/", function(req, res) {
+    Hike.find({}, function(err, allHikes) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("hikes/hikes", {hikes: allHikes, currentUser: req.user});
+        }
+    })
+    // res.render("hikes", {hikes: hikes});
+});
+
+router.post("/", function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var newHike = {name : name, image : image};
+    Hike.create(newHike, function(err, newlyCreated) {
+        if (err) {
+            console.log("Hike not added.")
+        } else {
+            console.log(newlyCreated);
+            res.redirect("/hikes");
+        }
+    });
+});
+
+
+
+router.get("/new", function(req, res) {
+    res.render("hikes/new");
+})
+
+// SHOW
+
+router.get("/:id", function(req, res) {
+    Hike.findById(req.params.id).populate("comments").exec((function (err, foundHike) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(foundHike);
+            res.render("hikes/show", { hike : foundHike });
+        }
+    }));
+});
+
+module.exports = router;
